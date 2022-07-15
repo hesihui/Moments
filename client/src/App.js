0798e266673f77be132eba970abd87ac531fa76c
Sihui He <sardarbinarytree@gmail.com>
2022-07-15 17:16:08 -0400

    v2
    fix some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import { getPosts } from './actions/posts';
+
 import memories from "./images/memories.png";
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -9,6 +12,12 @@ import useStyles from "./styles";
 
 function App() {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    },[dispatch])
+
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
@@ -16,7 +25,7 @@ function App() {
                 <img className={classes.image} src={memories} alt="memories" height="60"/>
             </AppBar>
             <Grow in>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
                     <Grid item xs={12} sm={7}>
                         <Posts />
                     </Grid>
--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-// set up the starting path
-// localhost:5000/posts
-app.use('/posts', postRoutes);
-
 // set up uploaded limit: not excceed 30mb
 app.use(bodyParse.json({ limit: "30mb", extended: true }));
 app.use(bodyParse.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
+// set up the starting path
+// localhost:5000/posts
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
     "mongodb+srv://sardar:sardar960702@cluster0.sawzgaz.mongodb.net/?retryWrites=true&w=majority";
